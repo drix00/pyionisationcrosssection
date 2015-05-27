06a3c8c502c6dcf0d0d567bc6d759b871509a81f
Hendrix Demers <12611589+drix00@users.noreply.github.com>
2015-05-27 17:05:02 -0400

    Add __init__ file to create the pyIonizationCrossSection package.

---
+++ pyIonisationCrossSection/__init__.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+"""
+.. py:currentmodule:: pyIonisationCrossSection.__init__
+.. moduleauthor:: Hendrix Demers <hendrix.demers@mail.mcgill.ca>
+
+
+"""
+
+# Script information for the file.
+__author__ = "Hendrix Demers (hendrix.demers@mail.mcgill.ca)"
+__version__ = "0.1"
+__date__ = "May 27, 2015"
+__copyright__ = "Copyright (c) 2015 Hendrix Demers"
+__license__ = "GPL 3"
