e1cde6a7b8c7574a1d82b8c825ae8b31fd1cd187
Hendrix Demers <12611589+drix00@users.noreply.github.com>
2017-02-08 06:31:35 -0500

    Start setup project script.

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='pyionisationcrosssection',
+    version='0.1',
+    packages=['pyIonisationCrossSection'],
+    url='',
+    license='Apache License, Version 2.0',
+    author='Hendrix Demers',
+    author_email='hendrix.demers@mail.mcgill.ca',
+    description='Electron ionization cross section models'
+)
