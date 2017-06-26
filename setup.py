6a54bf8af8f06deb1e74a2c9c2607f72cddb32b9
hendrix_demers <hendrix_demers@localhost>
2017-06-26 14:29:52 -0400

    Ignore build folder.

--- setup.py
+++ setup.py
@@ -1,9 +1,46 @@
-from distutils.core import setup
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+.. py:currentmodule:: setup
+
+.. moduleauthor:: Hendrix Demers <hendrix.demers@mail.mcgill.ca>
+
+Setup script for the project pyionizationcrosssection.
+"""
+
+###############################################################################
+# Copyright 2017 Hendrix Demers
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+###############################################################################
+
+# Standard library modules.
+import os
+
+# Third party modules.
+from setuptools import setup, find_packages
+
+# Local modules.
+
+# Globals and constants variables.
+
+packages = find_packages()
 
 setup(
     name='pyionisationcrosssection',
     version='0.1',
-    packages=['pyIonisationCrossSection'],
+    packages=packages,
     url='',
     license='Apache License, Version 2.0',
     author='Hendrix Demers',
